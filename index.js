c33d584e0d5819915283509f3fd17a9a9de447b3
Chris Jones <chrisjones0517@gmail.com>
2018-08-09 16:08:28 -0500

    added simple data visualization

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const express = require('express');
 const app = express();
 const Nightmare = require('nightmare');
 const vo = require('vo');
-// const nightmare = Nightmare({ show: true });
-const state = 'TX';
-const city = 'Houston';
-const url = `https://www.indeed.com/jobs?q=web+developer&l=${city}%2C+${state}`;
-// const selector = '#resultsCol';
-let countArray = [[0, 'javascript'], [0, 'node'], [0, 'php'], [0, 'html'], [0, 'react'], [0, 'vue'], [0, 'css'], [0, 'boostrap'], [0, 'mongo'], [0, 'sql'], [0, 'angular'], [0, 'c#'], [0, '.net'], [0, 'python'], [0, 'java'], [0, 'shell'], [0, 'c\\+\\+'], [0, ' c '], [0, 'typescript'], [0, 'ruby'], [0, 'swift'], [0, 'wordpress'], [0, ' go '], [0, 'assembly'], [0, 'matlab'], [0, ' r '], [0, 'kotlin'], [0, 'scala'], [0, 'groovy'], [0, 'perl'], [0, 'django'], [0, 'magento'], [0, 'laravel'], [0, 'redux'], [0, 'graphql']];
+const state = 'TX'; // Add the state of your choice here in the same 2 letter uppercase format
+const city = 'Houston'; // Add the city of your choice here
+const url = `https://www.indeed.com/jobs?q=web+developer&l=${city}%2C+${state}`; // The query value can be changed to the keyword of your choice by changing 'web+developer' to 'your+query+string'
+let countArray = [[0, 'javascript'], [0, 'node'], [0, 'php'], [0, 'html'], [0, 'react'], [0, 'vue'], [0, 'css'], [0, 'boostrap'], [0, 'mongo'], [0, 'sql'], [0, 'angular'], [0, 'c#'], [0, '.net'], [0, 'python'], [0, 'java'], [0, 'shell'], [0, 'c\\+\\+'], [0, ' c '], [0, 'typescript'], [0, 'ruby'], [0, 'swift'], [0, 'wordpress'], [0, ' go '], [0, 'assembly'], [0, 'matlab'], [0, ' r '], [0, 'kotlin'], [0, 'scala'], [0, 'groovy'], [0, 'perl'], [0, 'django'], [0, 'magento'], [0, 'laravel'], [0, 'redux'], [0, 'graphql']]; // Keywords can be added or deleted to suit preferences
 let results;
 vo(run)(function (err, result) {
     if (err) throw err;
 });
 
 function* run() {
-    let nightmare = Nightmare(),
+    let nightmare = Nightmare({
+        waitTimeout: 45000
+    }),
         MAX_PAGE = 100,
         currentPage = 0,
         nextExists = true,
@@ -28,9 +28,6 @@ function* run() {
     while (nextExists && currentPage < MAX_PAGE) {
         data.push(yield nightmare.evaluate(function () {
             let data = document.querySelector('#resultsCol').innerText;
-
-            
-            // console.log('data', data);
             return data;
         }));
 
@@ -41,7 +38,7 @@ function* run() {
         currentPage++;
         nextExists = yield nightmare.visible('.pagination a:last-child');
     }
-    // console.log('data near bottom', data);
+    
     keywordCount(data);
     yield nightmare.end();
 }
@@ -52,8 +49,6 @@ function keywordCount(text) {
     for (let i = 0; i < countArray.length; i++) {
         let tech = countArray[i][1];
         let regex = new RegExp(tech, 'gi');
-
-        console.log('regex', regex);
         let currentCount;
         if (text.match(regex)) {
             currentCount = text.match(regex).length;
@@ -61,10 +56,9 @@ function keywordCount(text) {
             currentCount = 0;
         }
         countArray[i][0] = currentCount;
-        console.log(currentCount);
     }
     results = countArray;
-    console.log(countArray);
+    console.log(results);
 }
 
 app.get('/', (req, res) => {
@@ -74,4 +68,5 @@ app.get('/', (req, res) => {
 app.get('/data', (req, res) => {
     res.json(results);
 });
+
 app.listen(3000);
