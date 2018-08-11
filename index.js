b0bf5b3f00a1543d62c303b520650111d4fde8dd
Chris Jones <chrisjones0517@gmail.com>
2018-08-11 16:01:21 -0500

    added a couple of technologies to the array

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const state = 'TX'; // Add the state of your choice here in the same 2 letter up
 const city = 'Houston'; // Add the city of your choice here
 const query = 'web+developer'; // The query value can be changed to the keyword of your choice by changing 'web+developer' to 'your+query+string'
 const url = `https://www.indeed.com/jobs?q=${query}&l=${city}%2C+${state}`; 
-let countArray = [[0, 'javascript'], [0, 'node'], [0, 'php'], [0, 'html'], [0, 'react'], [0, ' vue'], [0, 'css'], [0, 'bootstrap'], [0, 'mongo'], [0, 'sql'], [0, 'angular'], [0, 'c#'], [0, '.net'], [0, 'python'], [0, ' java '], [0, 'shell'], [0, 'c\\+\\+'], [0, ' c '], [0, 'typescript'], [0, 'ruby'], [0, 'swift'], [0, 'wordpress'], [0, ' go '], [0, 'assembly'], [0, 'matlab'], [0, ' r '], [0, 'kotlin'], [0, 'scala'], [0, 'groovy'], [0, 'perl'], [0, 'django'], [0, 'magento'], [0, 'laravel'], [0, 'redux'], [0, 'graphql']]; // Keywords can be added or deleted to suit preferences
+let countArray = [[0, 'javascript'], [0, 'node'], [0, 'php'], [0, 'html'], [0, 'react'], [0, ' vue'], [0, 'css'], [0, 'bootstrap'], [0, 'mongo'], [0, 'sql'], [0, 'angular'], [0, 'c#'], [0, '.net'], [0, 'python'], [0, ' java '], [0, 'shell'], [0, 'c\\+\\+'], [0, ' c '], [0, 'typescript'], [0, 'ruby'], [0, 'swift'], [0, 'wordpress'], [0, ' go '], [0, 'assembly'], [0, 'matlab'], [0, ' r '], [0, 'kotlin'], [0, 'scala'], [0, 'groovy'], [0, 'perl'], [0, 'django'], [0, 'magento'], [0, 'laravel'], [0, 'redux'], [0, 'graphql'], [0, 'ruby'], [0, 'joomla'], [0, 'drupal']]; // Keywords can be added or deleted to suit preferences
 let results;
 vo(run)(function (err, result) {
     if (err) throw err;
